b0713a5731f1d559d7fff2378f9caa3b82ef1199
Jory <bennett.jory.21@gmail.com>
2024-02-28 17:58:25 -0500

    Update Account in Queries

--- api/queries/accounts.py
+++ api/queries/accounts.py
@@ -28,3 +28,15 @@ class AccountQueries(Queries):
         self.collection.insert_one(account)
         account["id"] = str(account["_id"])
         return Account(**account)
+
+    def update(self, account_id: str, account_in: AccountIn):
+        query = {
+            'id': ObjectId{account_id}
+            'account_id': account_id
+        }
+        changes = account_in.dict()
+        res = self.collection.update_one(query, {'$set': changes})
+        if res.matched_count >= 1:
+            changes['id'] = account_id
+            changes['account_id'] = account_id
+            return changes
