9841cc3a44ee904b46b154f5deb0a802d1abb4cb
Jory <bennett.jory.21@gmail.com>
2024-03-01 15:13:51 -0500

    Routines All Endpoint and Functionality

--- api/queries/routines.py
+++ api/queries/routines.py
@@ -1,45 +1,55 @@
- from bson.objectid import ObjectId
-# from bson.errors import InvalidId
-# from .queries import Queries
-# from models import RoutineOut, RoutineIn
-# from typing import List
+from bson.objectid import ObjectId
+from bson.errors import InvalidId
+from .queries import Queries
+from models import RoutineOut, RoutineIn
+from typing import List
 
-# class RoutineQueries(Queries):
-#     DB_NAME = "buffbunny_hop"
-#     COLLECTION = "routines"
+class RoutineQueries(Queries):
+    DB_NAME = "buffbunny_hop"
+    COLLECTION = "routines"
 
-#     def get(self, routine_id: str):
-#         try:
-#             routine = self.collection.find_one({"_id": ObjectId(routine_id)})
-#             if routine is None:
-#                 return {"message": "Invalid routine ID"}
-#             routine["id"] = str(routine["_id"])
-#             return RoutineOut(routine)
-#         except InvalidId:
-#             return {"message": "Invalid routine ID"}
-#         except Exception as e:
-#             return {"message": "Unable to get workout, "+ str(e)}
+    def get(self, routine_id: str):
+        try:
+            routine = self.collection.find_one({"_id": ObjectId(routine_id)})
+            if routine is None:
+                return {"message": "Invalid routine ID"}
+            routine["id"] = str(routine["_id"])
+            return RoutineOut(routine)
+        except InvalidId:
+            return {"message": "Invalid routine ID"}
+        except Exception as e:
+            return {"message": "Unable to get workout, "+ str(e)}
 
-#     def update(self, routine_id: str, account_id: str, routine_in: RoutineIn):
-#         query = {
-#             '_id': ObjectId(routine_id),
-#             'account_id': account_id
-#         }
-#         changes = routine_in.dict()
-#         res = self.collection.update_one(query, {'$set': changes})
-#         if res.matched_count >= 1:
-#             changes['id'] = routine_id
-#             changes['account_id'] = account_id
-#             return changes
+    def get_all_routines(self, routine_id: str):
+        try:
+            routines_list = []
+            for routine in self.collection.find():
+                routine["id"] = str(routine["_id"])
+                routines_list.append(routine)
+            return routines_list
+        except Exception as e:
+            return {"message": "Unable to get exercises " + str(e)}
 
-#     def delete_routine(self, routine_id: str, account_id: str):
-#         try:
-#             result = self.collection.delete_one({"id": ObjectId(routine_id), "account_id": account_id})
-#             if result.deleted_count > 0:
-#                 return {"deleted": True}
-#             else:
-#                 return {"deleted": False, "message": "No such routine exists for this account"}
-#         except InvalidId:
-#             return {"deleted": False, "message": "Invalid routine ID"}
-#         except Exception as e:
-#             return {"deleted": False, "message": f"Error deleting routine: {str(e)}"}
+    def update(self, routine_id: str, account_id: str, routine_in: RoutineIn):
+        query = {
+            '_id': ObjectId(routine_id),
+            'account_id': account_id
+        }
+        changes = routine_in.dict()
+        res = self.collection.update_one(query, {'$set': changes})
+        if res.matched_count >= 1:
+            changes['id'] = routine_id
+            changes['account_id'] = account_id
+            return changes
+
+    def delete_routine(self, routine_id: str, account_id: str):
+        try:
+            result = self.collection.delete_one({"id": ObjectId(routine_id), "account_id": account_id})
+            if result.deleted_count > 0:
+                return {"deleted": True}
+            else:
+                return {"deleted": False, "message": "No such routine exists for this account"}
+        except InvalidId:
+            return {"deleted": False, "message": "Invalid routine ID"}
+        except Exception as e:
+            return {"deleted": False, "message": f"Error deleting routine: {str(e)}"}
---
+++ api/routers/routines.py
@@ -0,0 +1,52 @@
+from fastapi import (
+    Depends,
+    HTTPException,
+    status,
+    Response,
+    APIRouter,
+    Request,
+)
+from authenticator import authenticator
+from typing import List, Union
+from queries.routines import RoutineQueries
+from models import RoutineOut, RoutineIn, Error, Deleted
+
+router = APIRouter()
+
+@router.get("/api/routines/{routine_id}", response_model=Union[RoutineOut, Error])
+def get_routine(
+    routine_id,
+    account_data: dict = Depends(authenticator.get_current_account_data),
+    repo: RoutineQueries = Depends()
+):
+    routine = repo.get(routine_id)
+    return routine
+
+@router.get("/api/routines", response_model=Union[List[RoutineOut], Error])
+def get_all_routines(
+    account_id: dict = Depends(authenticator.get_current_account_data),
+    repo: RoutineQueries = Depends()
+):
+    routines_list = repo.get_all_routines()
+    return routines_list
+
+@router.put("/api/routines/{routine_id}", response_model=Union[RoutineOut, Error])
+def update_routine(
+    routine_id: str,
+    routine_in: RoutineIn,
+    account_id: dict = Depends(authenticator.get_current_account_data),
+    queries: RoutineQueries = Depends()
+):
+    routine = queries.update(routine_id=routine_id, account_id=account_id['id'], routine_in=routine_in)
+    if routine is None:
+        raise HTTPException(status_code=404, detail="Routine not found")
+    return routine
+
+@router.delete("/api/routines/{routine_id}", response_model=Deleted)
+def delete_routine(
+    routine_id: str,
+    account_id: dict = Depends(authenticator.get_current_account_data),
+    repo: RoutineQueries = Depends()
+):
+    deletion = repo.delete_routine(routine_id=routine_id, account_id=account_id["id"])
+    return deletion
