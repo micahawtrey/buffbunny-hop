c79bf230dd81a04303d0cad7813e8978d5cc50e1
Micah <micah.a.awtrey@gmail.com>
2024-03-05 16:19:32 -0700

    App.js modified

--- ghi/src/app/accountAPI.js
+++ ghi/src/app/accountAPI.js
@@ -3,7 +3,7 @@ import { createApi, fetchBaseQuery } from '@reduxjs/toolkit/query/react'
 export const accountApi = createApi({
     reducerPath: "accountApi",
     baseQuery: fetchBaseQuery({
-        baseUrl: process.env.VITE_API_HOST,
+        baseUrl: import.meta.env.VITE_API_HOST,
         credentials: "include"
     }),
     endpoints: (builder) => ({
