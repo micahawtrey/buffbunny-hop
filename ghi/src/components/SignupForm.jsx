f06e7cfeeaea11a16d92f251e5852729241e667a
Micah <micah.a.awtrey@gmail.com>
2024-03-14 10:09:47 -0600

    Fixed signup page errors

--- ghi/src/components/SignupForm.jsx
+++ ghi/src/components/SignupForm.jsx
@@ -1,7 +1,6 @@
-import React, { useEffect, useState } from 'react';
+import { useEffect, useState } from 'react';
 import { useCreateAccountMutation } from '../app/accountAPI';
 import { useNavigate } from 'react-router-dom';
-
 
 function SignupForm() {
   const [createAccount, createAccountStatus] = useCreateAccountMutation()
@@ -19,7 +18,7 @@ function SignupForm() {
   useEffect(() => {
     if (createAccountStatus.isSuccess) navigate("/dashboard")
     if (createAccountStatus.isError) {
-    setErrorMessage(createAccountStatus.error)
+    setErrorMessage(createAccountStatus.error.data.detail)
   }
   }, [createAccountStatus, navigate]);
 
@@ -28,7 +27,7 @@ function SignupForm() {
     if (formData.password === formData.password_confirmation) {
       createAccount(formData)
     }else{
-      setErrorMessage("Passwords do not match");
+      setErrorMessage("Passwords do not match.");
     }
   };
 
@@ -45,32 +44,29 @@ function SignupForm() {
   return (
     <div className="my-5 container">
       <div className="row">
-        <div className="col col-sm-auto">
-          <img width="300" className="bg-white rounded shadow d-block mx-auto mb-4" src="" alt="" />
-        </div>
         <div className="col">
-          <div className="card shadow">
+          <div className="card shadow mx-5">
             <div className="card-body">
               <h1 className="card-title">Sign Up</h1>
               <form onSubmit={handleSubmit} id="signup-form">
-                {errorMessage && <div className='alert alert-danger'>Your signup credentials did not match.</div>}
-                <div className="mb-3">
-                  <input onChange={handleFormChange} value={formData.name} required type='text' name='name' id='name' className='form-control' />
+                {errorMessage && <div className='alert alert-danger'>{errorMessage}</div>}
+                <div className="mb-3 form-floating">
+                  <input onChange={handleFormChange} value={formData.full_name} required type='text' name='full_name' id='full_name' className='form-control' />
                   <label htmlFor="name">Name</label>
                 </div>
-                <div className="mb-3">
-                  <input onChange={handleFormChange} value={formData.user_name} required type='text' name='user_name' id='user_name' className='form-control' />
+                <div className="mb-3 form-floating">
+                  <input onChange={handleFormChange} value={formData.username} required type='text' name='username' id='username' className='form-control' />
                   <label htmlFor="user_name">User Name</label>
                 </div>
-                <div className="mb-3">
-                  <input onChange={handleFormChange} value={formData.email_address} required type='email' name='email_address' id='email_address' className='form-control' />
+                <div className="mb-3 form-floating">
+                  <input onChange={handleFormChange} value={formData.email} required type='email' name='email' id='email' className='form-control' />
                   <label htmlFor="email_address">Email Address</label>
                 </div>
-                <div className="mb-3">
+                <div className="mb-3 form-floating">
                   <input onChange={handleFormChange} value={formData.password} required type='password' name='password' id='password' className='form-control' />
                   <label htmlFor="password">Password</label>
                 </div>
-                <div className="mb-3">
+                <div className="mb-3 form-floating">
                   <input onChange={handleFormChange} value={formData.password_confirmation} required type='password' name='password_confirmation' id='password_confirmation' className='form-control' />
                   <label htmlFor="password_confirmation">Password Confirmation</label>
                 </div>
