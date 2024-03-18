3c6b672090311e7939a2d1571f8825670b788727
Micah <micah.a.awtrey@gmail.com>
2024-03-18 16:43:20 -0600

    Workout creation styling

--- ghi/src/components/CreateWorkout.jsx
+++ ghi/src/components/CreateWorkout.jsx
@@ -122,71 +122,71 @@ function CreateWorkout (){
     }
 
    return (
-  <div style={{ paddingTop: '20px' }}>
-    <div className="form-floating mb-3">
-      <input type="text" className="form-control" id="workoutName" placeholder="Workout Name" name="name"
-        onChange={(event) => handleChange(event)} value={name}/>
-      <label htmlFor="workoutName">Workout Name</label>
-    </div>
-    <div className="form-floating mb-3">
-      <textarea className="form-control" id="workoutDescription" placeholder="Workout Description" name="description"
-       onChange={(event) => handleChange(event)} value={description}></textarea>
-      <label htmlFor="workoutDescription">Workout Description</label>
-    </div>
-    <div className="d-flex flex-wrap" style={{ border: '1px solid #ddd', borderRadius: '10px', padding: '10px' }}>
-        {Object.values(exercises).map(exercise => {
-            const exerciseKey = exercise.exerciseKey
-            return (
-            <div className="d-flex flex-grow-1" key={exercise.exerciseKey}>
-                <div className="form-group w-50">
-                    <select
-                        className="form-select"
-                        aria-label="Select Muscle Group"
-                        name="muscle_group"
-                        onChange={(event) => {
-                            setExerciseValues(event, exerciseKey)
-                            filterMuscleGroup(event.target.value, exerciseKey)
-                            }}>
-                            <option value="">Select Muscle Group</option>
-                            {targets.map((target, index) => (
-                                <option key={index} value={target}>{target}</option>
+    <div className='pt-3 text-center'>
+        <h1>Create a new Workout</h1>
+        <div className="form-floating my-3">
+            <input type="text" className="form-control" id="workoutName" placeholder="Workout Name" name="name"
+                onChange={(event) => handleChange(event)} value={name}/>
+            <label htmlFor="workoutName">Workout Name</label>
+        </div>
+        <div className="form-floating mb-3">
+            <textarea className="form-control" id="workoutDescription" placeholder="Workout Description" name="description"
+            onChange={(event) => handleChange(event)} value={description}></textarea>
+            <label htmlFor="workoutDescription">Workout Description</label>
+        </div>
+        <div className="d-flex flex-wrap border rounded" >
+            {Object.values(exercises).map(exercise => {
+                const exerciseKey = exercise.exerciseKey
+                return (
+                <div className="d-flex m-2" key={exercise.exerciseKey}>
+                    <div className="form-group w-50 me-2" >
+                        <select
+                            className="form-select" style={{height: 58}}
+                            aria-label="Select Muscle Group"
+                            name="muscle_group"
+                            onChange={(event) => {
+                                setExerciseValues(event, exerciseKey)
+                                filterMuscleGroup(event.target.value, exerciseKey)
+                                }}>
+                                <option value="">Select Muscle Group</option>
+                                {targets.map((target, index) => (
+                                    <option key={index} value={target}>{target}</option>
+                                ))}
+                        </select>
+                    </div>
+                    <div className="form-group w-50 me-2">
+                        <select
+                            className="form-select" style={{height: 58}}
+                            aria-label="Select Exercise" name="name"
+                            onChange={(event) => setExerciseValues(event, exerciseKey)}>
+                            <option defaultValue>Select Exercise</option>
+                            {exercise.exercises.map(exercise => (
+                                <option key={exercise.url} value={exercise.name}>{exercise.name}</option>
                             ))}
-                    </select>
+                        </select>
+                    </div>
+                    <div className="d-flex">
+                        <div className="form-floating me-2">
+                            <input type="number" className="form-control form-control-sm" id="sets" placeholder="Sets" min="1"
+                            name="set" value={exercises[exerciseKey]["set"]}
+                            onChange={(event) => setExerciseValues(event, exerciseKey) }/>
+                            <label htmlFor="sets">Sets</label>
+                        </div>
+                        <div className="form-floating me-2">
+                            <input type="number" className="form-control form-control-sm" id="reps" placeholder="Reps" min="1"
+                                name="rep" value={exercises[exerciseKey]["rep"]}
+                                onChange={(event) => setExerciseValues(event, exerciseKey)} />
+                            <label htmlFor="reps">Reps</label>
+                        </div>
+                    </div>
+                    <button className='btn btn-danger' onClick={() => removeExercise(exerciseKey)}>Delete</button>
                 </div>
-                <div className="form-group w-50">
-                    <select className="form-select" aria-label="Select Exercise" name="name"
-                        onChange={(event) => setExerciseValues(event, exerciseKey)}>
-                        <option defaultValue>Select Exercise</option>
-                        {exercise.exercises.map(exercise => (
-                            <option key={exercise.url} value={exercise.name}>{exercise.name}</option>
-                        ))}
-                    </select>
-                </div>
-                <div className="ms-4 d-flex">
-                <div className="form-floating me-2">
-                    <input type="number" className="form-control form-control-sm" id="sets" placeholder="Sets" min="1"
-                    name="set" value={exercises[exerciseKey]["set"]}
-                    onChange={(event) => setExerciseValues(event, exerciseKey) }/>
-                    <label htmlFor="sets">Sets</label>
-                </div>
-                <div className="form-floating">
-                    <input type="number" className="form-control form-control-sm" id="reps" placeholder="Reps" min="1"
-                    name="rep" value={exercises[exerciseKey]["rep"]}
-                    onChange={(event) => setExerciseValues(event, exerciseKey)} />
-                    <label htmlFor="reps">Reps</label>
-                </div>
-                </div>
-                <button className='btn btn-danger'
-                    onClick={() => removeExercise(exerciseKey)}> Delete Exercise</button>
-            </div>
-            )
-        })}
-        <button className="btn btn-primary"
-            onClick={() => addNewExercise()}>Add New Exercise</button>
+                )
+            })}
+            <button className="btn btn-primary m-2" onClick={() => addNewExercise()}>Add New Exercise</button>
+        </div>
+        <button className='btn btn-success m-2'onClick={() => handleSubmit()}> Create Workout</button>
     </div>
-    <button className='btn btn-success'
-        onClick={() => handleSubmit()}> Create Workout</button>
-  </div>
 );
 
 }
