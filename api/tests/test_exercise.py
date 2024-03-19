1e435f46a322e16f012074e00f696e053afc5405
Trevor Cannatella <tjc1295@gmail.com>
2024-03-19 18:08:33 +0000

    Resolve "Test cases"

--- api/tests/test_exercise.py
+++ api/tests/test_exercise.py
@@ -1,25 +1,44 @@
 from fastapi.testclient import TestClient
 from queries.exercises import ExerciseQueries
+from authenticator import authenticator
 from main import app
 
 
 client = TestClient(app=app)
 
+def fake_get_current_account_data():
+    return{
+        "id": "FAKE_ACCOUNT_ID"
+    }
 
 class FakeExerciseQueries:
-    def filtered_exercises(self,name, muscle_group):
+    def create_exercise(self, exercise_in, account_id):
         exercise = exercise_in.dict()
+        exercise['account_id'] = account_id
+        exercise['id'] = 'FAKE_ID'
+        return exercise
 
-        return []
 
-def test_filter_exercises():
+
+def test_create_exercise():
     #Arrange
     app.dependency_overrides[ExerciseQueries] = FakeExerciseQueries
+    app.dependency_overrides[authenticator.get_current_account_data] = (
+        fake_get_current_account_data
+
+     )
 
     #Act
-    exercise_in: {"name": "bench"}
-    res = client.get('/api/exercises')
+    exercise_in = {
+        "name": "bech",
+        "muscle_group": "chest",
+    }
+    res = client.post('/api/exercises', json=exercise_in)
 
     #Assert
     assert res.status_code == 200
-    assert res.json == 
+    assert res.json() == {
+        "name": "bech",
+        "muscle_group": "chest",
+        "id": "FAKE_ID",
+    }
